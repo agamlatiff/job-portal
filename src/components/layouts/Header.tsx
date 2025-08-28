945c692a57090920518a20e2a7d66853e40eb964
agamlatiff <agam.latiff@gmail.com>
2025-08-29 02:20:14 +0700

    Slicing Dashboard Company (3)

---
+++ src/components/layouts/Header.tsx
@@ -0,0 +1,19 @@
+import { PlusIcon } from "lucide-react";
+import { Button } from "../ui/button";
+
+const Header = () => {
+  return <div className="pb-3 mb-8 border-b border-border flex flex-row items-center justify-between">
+    <div>
+      <div>Company</div>
+      <div className="font-semibold">Twitter</div>
+    </div>
+    <div>
+      <Button className="rounded-none py-3 px-6">
+        <PlusIcon className="mr-2 w-4 h-4"/>
+        Post a job
+      </Button>
+    </div>
+  </div>;
+};
+
+export default Header;
