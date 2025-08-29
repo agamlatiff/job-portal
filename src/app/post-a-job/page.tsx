5bafcd9156e90bedef7e93f0a66dfe71f31080f5
agamlatiff <agam.latiff@gmail.com>
2025-08-29 13:28:35 +0700

    Slicing Dashboard Company (6

--- src/app/post-a-job/page.tsx
+++ src/app/post-a-job/page.tsx
@@ -1,5 +1,18 @@
+"use client";
+
+import FieldInput from "@/components/layouts/FieldInput";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Separator } from "@/components/ui/separator";
 import { jobFormSchema } from "@/lib/schema";
 import { zodResolver } from "@hookform/resolvers/zod";
+import { ArrowLeftIcon } from "lucide-react";
 import { useForm } from "react-hook-form";
 import type z from "zod";
 
@@ -7,15 +20,48 @@ const PostJobPage = () => {
   const form = useForm<z.infer<typeof jobFormSchema>>({
     resolver: zodResolver(jobFormSchema),
     defaultValues: {
-      requiredSkills : []
-    }
-  })
-  
-  const onSubmit = (val: z.infer<typeof jobFormSchema>) => {
-    
-  }
-  
-  return <div>PostJobPage</div>;
+      requiredSkills: [],
+    },
+  });
+
+  const onSubmit = (val: z.infer<typeof jobFormSchema>) => {};
+
+  return (
+    <div>
+      <div className="inline-flex items-center gap-2 cursor-pointer hover:text-primary">
+        <ArrowLeftIcon className="size-7" />
+        <span className="text-2xl font-semibold">Post a Job</span>
+      </div>
+      <div className="my-5">
+        <div className="text-lg font-semibold">Basic Information</div>
+        <div className="text-gray-400">
+          List out your top perks and benefits.
+        </div>
+      </div>
+      <Separator />
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="mt-5 space-y-6 pt-6">
+          <FieldInput
+            title="Job Title"
+            subtitle="Job title must be describe one position"
+          >
+            <FormField
+              control={form.control}
+              name="roles"
+              render={({ field }) => (
+                <FormItem>
+                  <FormControl>
+                    <Input placeholder="e.g. Software Engineer" {...field}  className="w-[450px]"/>
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          </FieldInput>
+        </form>
+      </Form>
+    </div>
+  );
 };
 
 export default PostJobPage;
