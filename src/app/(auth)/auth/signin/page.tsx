25854b53fcccb92682ef5cc8b1f49947abed1a46
agamlatiff <agam.latiff@gmail.com>
2025-09-01 13:27:14 +0700

    Slicing Dashboard Company (22)

---
+++ src/app/(auth)/auth/signin/page.tsx
@@ -0,0 +1,11 @@
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: 'Sign In'
+}
+
+const SignInPage = () => {
+  return <div>SignInPage</div>;
+};
+
+export default SignInPage;
---
+++ src/app/(auth)/auth/signup/page.tsx
@@ -0,0 +1,11 @@
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Sign In",
+};
+
+const SignUpPage = () => {
+  return <div>SignUpPage</div>;
+};
+
+export default SignUpPage;
