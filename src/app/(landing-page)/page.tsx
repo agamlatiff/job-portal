c2381e124fa5c7de4fe4db5d5ecea6edbec79026
agamlatiff <agam.latiff@gmail.com>
2025-09-03 11:44:33 +0700

    Slicing Landing Page (5)

--- src/app/(landing-page)/page.tsx
+++ src/app/(landing-page)/page.tsx
@@ -1,10 +1,12 @@
+import Clients from '@/components/page/Clients'
 import Hero from '@/components/page/Hero'
 import React from 'react'
 
 const LandingPage = () => {
   return (
-    <div className='px-32'>
+    <div className='px-32 mb-10'>
       <Hero/>
+      <Clients/>
     </div>
   )
 }
