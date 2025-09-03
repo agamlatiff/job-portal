85f4d25e65dc4af28878dbe45b6519a1aa5615e3
agamlatiff <agam.latiff@gmail.com>
2025-09-03 22:42:06 +0700

    Slicing Find Jobs Page (2)

--- src/app/containers/ExploreDataContainer.tsx
+++ src/app/containers/ExploreDataContainer.tsx
@@ -2,9 +2,9 @@ import Image from "next/image";
 import type { FC } from "react";
 
 interface ExploreDataContainerProps {
-  formFilter: any;
+  formFilter?: any;
   onSubmitFilter?: (val: any) => Promise<void>;
-  filterForms: any;
+  filterForms?: any;
 }
 
 const ExploreDataContainer: FC<ExploreDataContainerProps> = ({
@@ -13,32 +13,45 @@ const ExploreDataContainer: FC<ExploreDataContainerProps> = ({
   formFilter,
 }) => {
   return (
-    <div className="bg-gray-200 px-32 pt-16 pb-14">
-      <div className="mb-10">
-        <div className="mx-auto mb-11 text-center flex justify-center gap-2">
-          <span className="text-5xl font-semibold">Find Your</span>
-          <div className="relative">
-            <span className="text-5xl font-semibold text-primary">
-              dream job
-            </span>
-            <div className="absolute top-10 w-[220px] h-10">
-              <Image
-                src={"/images/pattern2.png"}
-                alt="pattern"
-                fill
-                objectFit="contain"
-              />
+    <>
+      <div className="bg-gray-200 px-32 pt-16 pb-14">
+        <div className="mb-10">
+          <div className="mx-auto mb-11 text-center flex justify-center gap-2">
+            <span className="text-5xl font-semibold">Find Your</span>
+            <div className="relative">
+              <span className="text-5xl font-semibold text-primary">
+                dream job
+              </span>
+              <div className="absolute top-10 w-[220px] h-10">
+                <Image
+                  src={"/images/pattern2.png"}
+                  alt="pattern"
+                  fill
+                  objectFit="contain"
+                />
+              </div>
             </div>
           </div>
+          <div className="text-center text-gray-500">
+            Find your next career at companies like HubSpot, Nike, and DropBox
+          </div>
         </div>
-        <div className="text-center text-gray-500">
-          Find your next career at companies like HubSpot, Nike, and DropBox
+        <div>
+          <div>Form Search</div>
         </div>
       </div>
-      <div>
-        <div>Form Search</div>
+
+      <div className="mt-20 mb-16 px-32 flex flex-row items-start gap-10">
+        <div>form filter data</div>
+        <div className="w-4/5">
+          <div className="mb-5">
+            <div className="text-3xl font-semibold">All Jobs</div>
+            <div className="text-muted-foreground">Showing 73 Result</div>
+          </div>
+          <div>Job Card</div>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
