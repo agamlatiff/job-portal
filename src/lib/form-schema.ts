7ed71d37df0122925dd1523928c33d779dbb9161
agamlatiff <agam.latiff@gmail.com>
2025-09-04 20:36:48 +0700

    Slicing Detail Job Page (4)

--- src/lib/form-schema.ts
+++ src/lib/form-schema.ts
@@ -1,9 +1,22 @@
 import z from "zod";
 
 export const formFilterSchema = z.object({
-  categories: z.array(z.string())
-})
+  categories: z.array(z.string()),
+});
 
 export const formFilterCompanySchema = z.object({
-  industry: z.array(z.string())
-})
+  industry: z.array(z.string()),
+});
+
+export const formApplySchema = z.object({
+  resume: z.any().refine((file: any) => file?.name, "Please Upload Resume"),
+  fullname: z
+    .string({ message: "Fullname is required" })
+    .min(5, { message: "Full name minimal 5 characters" }),
+    email: z.string({message: 'Email is required'}).email({message: 'Email not valid'}),
+    phone: z.string().min(6, {message:'Phone have min 6 characters'}),
+    previousJobTitle : z.string(), 
+    linkedIn : z.string(), 
+    portfolio : z.string(), 
+    coverLetter : z.string(), 
+});
